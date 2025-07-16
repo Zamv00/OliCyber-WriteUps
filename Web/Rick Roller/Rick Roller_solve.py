460465ad8e003fba12105cc5a5f455a526f555f8
Samuele Brandi <mappingfire052@gmail.com>
2025-07-16 12:51:47 +0200

    Solved✅

--- Web/Rick Roller/Rick Roller_solve.py
+++ Web/Rick Roller/Rick Roller_solve.py
@@ -8,5 +8,5 @@ r = requests.get(f"{site}/get_flag.php", allow_redirects=False)
 print(r.text)
 
 
-#ci basta visitare http://roller.challs.olicyber.it/get_flag.php senza seguire il redirect al rick roll
+#ci basta visitare http://roller.challs.olicyber.it/get_flag.php senza seguire il redirect al rick roll, si può anche fare più velocemente con curl "http://roller.challs.olicyber.it/get_flag.php" btw
 #flag{r1ck_4st1ey_c14ssic_m3m3}
